85bb73c2eb1dea3c50bb91d6ab33f674e6a5224a
Gaute Meek Olsen <gaute.meek@gmail.com>
2025-02-13 22:33:57 +0100

    log multiline without key prefix on same line

--- src/log.ts
+++ src/log.ts
@@ -1,14 +1,30 @@
 import { blue, cyan, magenta, red } from '@std/fmt/colors'
 
 export function logInfo(msg: string, options: { key: string, type: '1' | '2' | 'await' }) {
-  const colorFns = {
+  const colorFn = {
     '1': blue,
     '2': cyan,
     'await': magenta,
-  } as const
-  console.log(`${colorFns[options.type](`[${options.key}]`)} ${msg.trim()}`)
+  }[options.type]
+  msg = msg.trim()
+  if(isMultiline(msg)) {
+    console.log(colorFn(`[${options.key}] ▼`))
+    console.log(msg)
+  } else {
+    console.log(`${colorFn(`[${options.key}]`)} ${msg}`)
+  }
 }
 
 export function logError(msg: string, options: { key: string }) {
-  console.error(red(`[${options.key}] ${msg.trim()}`))
+  msg = msg.trim()
+  if(isMultiline(msg)) {
+    console.error(red(`[${options.key}] ▼`))
+    console.error(msg)
+  } else {
+    console.error(red(`[${options.key}] ${msg}`))
+  }
+}
+
+function isMultiline(msg: string) {
+  return msg.indexOf('\n') !== -1
 }
