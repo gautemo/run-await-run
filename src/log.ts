fc51b501f7ff3029b13ae6f4562fe8e594420e66
Gaute Meek Olsen <gaute.meek@gmail.com>
2025-02-14 10:51:29 +0100

    improve logs

--- src/log.ts
+++ src/log.ts
@@ -8,23 +8,27 @@ export function logInfo(msg: string, options: { key: string, type: '1' | '2' | '
   }[options.type]
   msg = msg.trim()
   if(isMultiline(msg)) {
-    console.log(colorFn(`[${options.key}] ▼`))
-    console.log(msg)
+    console.log(colorFn(`┌ ${options.key} ▾`))
+    console.log(prefixMultliline(msg, colorFn))
   } else {
-    console.log(`${colorFn(`[${options.key}]`)} ${msg}`)
+    console.log(`${colorFn(`${options.key} ▸`)} ${msg}`)
   }
 }
 
 export function logError(msg: string, options: { key: string }) {
   msg = msg.trim()
   if(isMultiline(msg)) {
-    console.error(red(`[${options.key}] ▼`))
-    console.error(msg)
+    console.error(red(`┌ ${options.key} ▾`))
+    console.error(prefixMultliline(msg, red))
   } else {
-    console.error(red(`[${options.key}] ${msg}`))
+    console.error(red(`${options.key} ▸ ${msg}`))
   }
 }
 
 function isMultiline(msg: string) {
   return msg.indexOf('\n') !== -1
+}
+
+function prefixMultliline(msg: string, colorFn: (str: string) => string) {
+  return msg.replaceAll(/^/gm, colorFn('│ ')).replace(/│(?!.*\n)/, '└')
 }
